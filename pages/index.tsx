9a01c4ea3069e1403d54dc25301ddc3ac3947ddd
kunalkeshan <kunalkeshan12@gmail.com>
2023-02-18 00:28:20 +0530

    chore: updated to next image and file sorting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
-import Head from "next/head";
-import Image from "next/image";
-import Donate from "./ngo/donate";
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Care For Them</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1 className="font-cutmark text-7xl bg-primaryBg h-screen ">
-        <Donate />
-      </h1>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Care For Them</title>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+		</>
+	);
 }
