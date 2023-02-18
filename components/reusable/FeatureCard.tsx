e4b7c49bd22b1026039d98f62a8dd6274172227e
kunalkeshan <kunalkeshan12@gmail.com>
2023-02-19 00:39:55 +0530

    chore: styling updates in features

--- components/reusable/FeatureCard.tsx
+++ components/reusable/FeatureCard.tsx
@@ -10,7 +10,7 @@ interface FeatureProps {
 const FeatureCard: React.FC<FeatureProps> = ({ Icon, title, description }) => {
 	return (
 		<div className='group flex h-full w-full flex-col gap-4 rounded-3xl bg-gradient-to-br from-gray-700 to-primary p-6 transition-all duration-300 hover:scale-[1.01] hover:shadow-md hover:shadow-gray-800'>
-			<div className='flex w-fit items-center justify-center rounded-full border-2 border-white border-opacity-40 bg-white bg-opacity-10 p-3 text-tertiary shadow-md shadow-black transition-all duration-300 group-hover:scale-[0.99] group-hover:shadow-inner'>
+			<div className='flex w-fit -skew-x-3 skew-y-3 items-center justify-center rounded-full border-2 border-white border-opacity-40 bg-white bg-opacity-10 p-3 text-tertiary shadow-md shadow-black transition-all duration-300 group-hover:scale-[0.99] group-hover:shadow-inner'>
 				<Icon className='text-7xl' />
 			</div>
 			<h3 className='font-cutmark text-lg font-medium md:text-2xl'>
