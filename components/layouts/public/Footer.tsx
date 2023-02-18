681493e2f3b004af541248550e4897e896fe746e
Mohammad Zeeshan Jawed <zeeshan8281@gmail.com>
2023-02-19 01:27:38 +0530

    Navbar and Footer

--- components/layouts/public/Footer.tsx
+++ components/layouts/public/Footer.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
 
 const Footer = () => {
-	return <div></div>;
+	return <div>
+		import React from 'react';
+
+function Footer() {
+  return (
+    <footer class="bg-gray-900 text-white">
+      <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+        <div class="flex-grow">
+          <h4 class="text-lg font-medium mb-2">Section 1</h4>
+          <div class="flex items-center mb-4">
+            <img class="h-8 w-8 mr-2" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Logo" />
+            <a class="hover:text-gray-500" href="https://github.com/your-github">
+              GitHub Link
+            </a>
+          </div>
+          <p class="text-sm">&copy; 2023 Your Company. All rights reserved.</p>
+        </div>
+        <div class="flex-grow">
+          <h4 class="text-lg font-medium mb-2">Section 2 - Page Links</h4>
+          <nav class="flex flex-wrap">
+            <a class="mr-4 mb-2 hover:text-gray-500" href="#">Link 1</a>
+            <a class="mr-4 mb-2 hover:text-gray-500" href="#">Link 2</a>
+            <a class="mr-4 mb-2 hover:text-gray-500" href="#">Link 3</a>
+            <a class="mr-4 mb-2 hover:text-gray-500" href="#">Link 4</a>
+          </nav>
+        </div>
+        <div class="flex-grow">
+          <h4 class="text-lg font-medium mb-2">Section 3 - Team Links</h4>
+          <nav class="flex flex-wrap">
+            <a class="mr-4 mb-2 hover:text-gray-500" href="#">Member 1</a>
+            <a class="mr-4 mb-2 hover:text-gray-500" href="#">Member 2</a>
+            <a class="mr-4 mb-2 hover:text-gray-500" href="#">Member 3</a>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+	</div>;
 };
 
 export default Footer;
--- components/layouts/public/Navbar.tsx
+++ components/layouts/public/Navbar.tsx
@@ -1,7 +1,86 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import Web3 from 'web3';
 
 const Navbar = () => {
-	return <div></div>;
+
+	return <div>
+			const Navbar = () => {
+  const handleConnectWallet = async() => {
+    if (typeof window.ethereum !== 'undefined') {
+        try {
+          // Request account access
+          await window.ethereum.request({ method: 'eth_requestAccounts' });
+          
+          // Get the user's address
+          const web3 = new Web3(window.ethereum);
+          const accounts = await web3.eth.getAccounts();
+          const address = accounts[0];
+    
+          // Store the user's address in state or local storage
+          // Redirect the user to a page for connected users
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        // Handle the case where Web3 is not available
+      }
+  };
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="flex-shrink-0 flex items-center">
+            {/* Your logo here */}
+            <Link to="/">
+              <img
+                className="h-8 w-auto justify-left"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                alt="Workflow"
+              />
+            </Link>
+            <Link
+                  to="/"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium w-auto"
+                >
+                  Home
+            </Link>
+            <Link
+                  to="/"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium w-auto"
+                >
+                  About
+            </Link>
+            <Link
+                  to="/"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium w-auto"
+                >
+                  Features
+            </Link>
+            <Link
+                  to="/"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium w-auto"
+                >
+                  Our Team
+            </Link>
+          </div>
+          <div className="hidden sm:block sm:ml-6">
+            <button
+              onClick={handleConnectWallet}
+              className="bg-indigo-500 hover:bg-blue-500 text-white py-2 px-4 rounded-md"
+            >
+              Connect Wallet
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+	</div>;
 };
 
 export default Navbar;
