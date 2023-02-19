f65ff52309bbc566e45bd920bd33d1802bcc5c4d
Kevin Paul <kpjtofficial@gmail.com>
2023-02-19 14:53:24 +0530

    chore: Nabar and Footer responsive and Links setup. (#30)
    * chore: Nabar and Footer responsive and Links setup.
    
    * fix: spellings
    
    * chore: altruize copyright updated
    
    * chore: bg for features in abotu
    
    * chore: headings aligned in left for landing
    
    ---------
    
    Co-authored-by: kunalkeshan <kunalkeshan12@gmail.com>

--- components/layouts/public/Footer.tsx
+++ components/layouts/public/Footer.tsx
@@ -1,12 +1,78 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import {
+	AiFillGithub,
+	AiOutlineLink,
+	AiFillLinkedin,
+	AiFillTwitterCircle,
+} from 'react-icons/ai';
 
 const Footer = () => {
+	const navOptions = [
+		{
+			name: 'Home',
+			url: '/',
+		},
+		{
+			name: 'About',
+			url: '/#about',
+		},
+		{
+			name: 'Features',
+			url: '/#features',
+		},
+		{
+			name: 'FAQs',
+			url: '/#faqs',
+		},
+		{
+			name: 'NGOs',
+			url: '/ngo',
+		},
+		{
+			name: 'Profile',
+			url: '/user',
+		},
+	];
+
+	const teamOptions = [
+		{
+			name: 'Kunal Keshan',
+			github: 'https://github.com/kunalkeshan',
+			linkedin: 'https://www.linkedin.com/in/kunalkeshan',
+			twitter: 'https://twitter.com/_kunalkeshan_',
+			portfolio: 'https://kunalkeshan.dev',
+		},
+		{
+			name: 'Kevin Paul',
+			github: 'https://github.com/itaintkevin',
+			linkedin: 'https://www.linkedin.com/in/itaintkevin',
+			twitter: 'https://twitter.com/it_aint_kevin',
+			portfolio: 'https://kevinpaul.xyz',
+		},
+		{
+			name: 'Zeeshan Javed',
+			github: 'https://github.com/zeeshan8281',
+			linkedin:
+				'https://www.linkedin.com/in/mohammad-zeeshan-jawed-bab51a1a6',
+			twitter:
+				'https://twitter.com/zeeshan_utd?t=ByuEiYw8co0PYTrS3eAUzw&s=09',
+			portfolio: '',
+		},
+		{
+			name: 'Arnav Shukla',
+			github: 'https://github.com/thelordzeus',
+			linkedin: 'https://www.linkedin.com/in/arnavshukla',
+			twitter: 'https://twitter.com/iamlordzeus',
+			portfolio: 'https://thelordzeus.vercel.app',
+		},
+	];
+
 	return (
-		<footer className='bg-primary text-white'>
+		<footer className='min-h-[20vh] bg-primary text-white'>
 			<div className='container mx-auto flex flex-col flex-wrap py-4 px-5 sm:flex-row'>
-				<div className='flex-grow'>
-					<h4 className='mb-2 text-lg font-medium'>Section 1</h4>
+				<div className='my-[2%] flex-grow'>
 					<div className='mb-4 flex items-center'>
 						<Image
 							className='mr-2 h-8 w-8'
@@ -16,49 +82,73 @@ const Footer = () => {
 							height={100}
 						/>
 						<a
-							className='hover:text-gray-500'
-							href='https://github.com/your-github'
+							className='text-xl font-semibold hover:text-gray-500'
+							href='https://github.com/zeeshan8281/Altruize'
 						>
-							GitHub Link
+							Altruize
 						</a>
 					</div>
 					<p className='text-sm'>
-						&copy; 2023 Your Company. All rights reserved.
+						&copy; 2023 Altruize. All rights reserved.
 					</p>
 				</div>
-				<div className='flex-grow'>
-					<h4 className='mb-2 text-lg font-medium'>
-						Section 2 - Page Links
-					</h4>
+				<div className='my-[2%] flex-grow'>
+					<h4 className='mb-2 text-lg font-medium'>Pages</h4>
 					<nav className='flex flex-wrap'>
-						<a className='mr-4 mb-2 hover:text-gray-500' href='#'>
-							Link 1
-						</a>
-						<a className='mr-4 mb-2 hover:text-gray-500' href='#'>
-							Link 2
-						</a>
-						<a className='mr-4 mb-2 hover:text-gray-500' href='#'>
-							Link 3
-						</a>
-						<a className='mr-4 mb-2 hover:text-gray-500' href='#'>
-							Link 4
-						</a>
+						{navOptions.map((option, index) => (
+							<>
+								<Link
+									key={index}
+									href={option.url}
+									className='w-auto rounded-md py-2 pr-3 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+								>
+									{option.name}
+								</Link>
+							</>
+						))}
 					</nav>
 				</div>
-				<div className='flex-grow'>
-					<h4 className='mb-2 text-lg font-medium'>
-						Section 3 - Team Links
-					</h4>
+				<div className='my-[2%] flex-grow'>
+					<h4 className='mb-2 text-lg font-medium'>Team</h4>
 					<nav className='flex flex-wrap'>
-						<a className='mr-4 mb-2 hover:text-gray-500' href='#'>
-							Member 1
-						</a>
-						<a className='mr-4 mb-2 hover:text-gray-500' href='#'>
-							Member 2
-						</a>
-						<a className='mr-4 mb-2 hover:text-gray-500' href='#'>
-							Member 3
-						</a>
+						{teamOptions.map((option, index) => (
+							<>
+								<ul
+									key={index}
+									className='w-auto rounded-md py-2 pr-3 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+								>
+									<li className='text-white'>
+										{option.name}
+									</li>
+									<li className='flex'>
+										<Link
+											href={option.github}
+											className='pr-2'
+										>
+											<AiFillGithub size={25} />
+										</Link>
+										<Link
+											href={option.linkedin}
+											className='pr-2'
+										>
+											<AiFillLinkedin size={25} />
+										</Link>
+										<Link
+											href={option.twitter}
+											className='pr-2'
+										>
+											<AiFillTwitterCircle size={25} />
+										</Link>
+										<Link
+											href={option.portfolio}
+											className='pr-2'
+										>
+											<AiOutlineLink size={25} />
+										</Link>
+									</li>
+								</ul>
+							</>
+						))}
 					</nav>
 				</div>
 			</div>
